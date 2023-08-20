afee1d04e1489c2a15f49d1a2cc058bbc24fe618
Anshh Chaturvedi <72307339+Anshh1010@users.noreply.github.com>
2023-08-21 02:32:49 +0530

    Update dalle.routes.js

--- server/routes/dalle.routes.js
+++ server/routes/dalle.routes.js
@@ -54,5 +54,34 @@ router.route('/').post(async(req, res) => {
         res.status(500).json({ message: "Something went wrong" })
     }
 })
+router.route('/').post(async(req, res) => {
+    try {
+        const { prompt } = req.body;
+        console.log(prompt);
+        console.log("Going")
+        const pythonScript = spawn('python3', ['/Users/anshhchaturvedi/Desktop/project_threejs_ai/server/routes/pinterest.py', prompt]);
+        console.log("spawn done")
+        pythonScript.stdout.on('data', (data) => {
+            console.log(`Python script output: ${data}`);
+        });
 
+        pythonScript.stderr.on('data', (data) => {
+            console.error(`Python script error: ${data}`);
+        });
+
+        pythonScript.on('close', (code) => {
+            console.log(`Python script exited with code ${code}`);
+
+            const paths = [
+                'output.png'
+            ];
+
+            res.json({ paths });
+        });
+        res.status(200).json({ message: prompt });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Something went wrong" })
+    }
+})
 export default router;
